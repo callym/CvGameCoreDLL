ddfda7ab3034ccd9fab2bbab70163dba67868e6e
Callym <hi@callym.com>
2022-03-04 11:10:48 +0000

    Remove unneeded `DllExport`

--- CvGameCoreDLL/FAssert.h
+++ CvGameCoreDLL/FAssert.h
@@ -14,7 +14,7 @@
 
 #ifdef WIN32
 
-extern "C" DllExport bool FAssertDlg(const char *, const char *, const char *, unsigned int, bool &);
+extern "C" bool FAssertDlg(const char *, const char *, const char *, unsigned int, bool &);
 
 #define FAssert(expr)                                                                                                  \
   {                                                                                                                    \
